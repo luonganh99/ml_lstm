56f775f3f61d9d52c395a4c1d47a1b4b7d6852d0
thaison247 <nguyenthaison247@gmail.com>
2021-07-18 16:07:23 +0700

    Update price rate of change Apple

--- stock_app.py
+++ stock_app.py
@@ -6,67 +6,17 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential  # Deeplearing API
+from keras.layers import LSTM, Dropout, Dense
 import numpy as np
-
+import apple_close
+import tesla_close
 
 app = dash.Dash()
 server = app.server
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-# lay 8365 dau tien
-df_nse = pd.read_csv("./NSE-TATA.csv")
-
-df_nse["Date"] = pd.to_datetime(df_nse.Date, format="%Y-%m-%d") # doi format sang %d/%m/%Y
-df_nse.index = df_nse["Date"]
-
-
-data = df_nse.sort_index(ascending=True, axis=0) 
-new_data = pd.DataFrame(index=range(0, len(df_nse)), columns=["Date", "Close"])
-
-for i in range(0, len(data)):
-    new_data["Date"][i] = data["Date"][i]
-    new_data["Close"][i] = data["Close"][i]
-
-new_data.index = new_data.Date
-new_data.drop("Date", axis=1, inplace=True)
-
-dataset = new_data.values
-
-train = dataset[0:987, :]
-valid = dataset[987:, :]
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(dataset)
-
-x_train, y_train = [], []
-
-for i in range(60, len(train)):
-    x_train.append(scaled_data[i - 60 : i, 0])
-    y_train.append(scaled_data[i, 0])
-
-x_train, y_train = np.array(x_train), np.array(y_train)
-
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-model = load_model("saved_lstm_model.h5")
-
-inputs = new_data[len(new_data) - len(valid) - 60 :].values
-inputs = inputs.reshape(-1, 1)
-inputs = scaler.transform(inputs)
-
-X_test = []
-for i in range(60, inputs.shape[0]):
-    X_test.append(inputs[i - 60 : i, 0])
-X_test = np.array(X_test)
-
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-closing_price = model.predict(X_test)
-closing_price = scaler.inverse_transform(closing_price)
-
-train = new_data[:987]
-valid = new_data[987:]
-valid["Predictions"] = closing_price
+# teslaCloseData = tesla_close.train()
+appleCloseData = apple_close.train()
 
 
 df = pd.read_csv("./stock_data.csv")
@@ -78,7 +28,7 @@ app.layout = html.Div(
             id="tabs",
             children=[
                 dcc.Tab(
-                    label="NSE-TATAGLOBAL Stock Data",
+                    label="Apple Stock Data",
                     children=[
                         html.Div(
                             [
@@ -87,12 +37,12 @@ app.layout = html.Div(
                                     style={"textAlign": "center"},
                                 ),
                                 dcc.Graph(
-                                    id="Actual Data",
+                                    id="Actual Apple Data",
                                     figure={
                                         "data": [
                                             go.Scatter(
-                                                x=train.index,
-                                                y=valid["Close"],
+                                                x=appleCloseData[0].index,
+                                                y=appleCloseData[1]["Close"],
                                                 mode="markers",
                                             )
                                         ],
@@ -112,8 +62,8 @@ app.layout = html.Div(
                                     figure={
                                         "data": [
                                             go.Scatter(
-                                                x=valid.index,
-                                                y=valid["Predictions"],
+                                                x=appleCloseData[0].index,
+                                                y=appleCloseData[1]["Predictions"],
                                                 mode="markers",
                                             )
                                         ],
@@ -124,63 +74,154 @@ app.layout = html.Div(
                                         ),
                                     },
                                 ),
+                                html.H2(
+                                    "Actual price rate of change",
+                                    style={"textAlign": "center"},
+                                ),
+                                dcc.Graph(
+                                    id="Actual price rate of change Apple Data",
+                                    figure={
+                                        "data": [
+                                            go.Scatter(
+                                                x=appleCloseData[2].index,
+                                                y=appleCloseData[3]["Rate"],
+                                            )
+                                        ],
+                                        "layout": go.Layout(
+                                            title="scatter plot",
+                                            xaxis={"title": "Date"},
+                                            yaxis={"title": "Rate"},
+                                        ),
+                                    },
+                                ),
+                                html.H2(
+                                    "LSTM Predicted price rate of change",
+                                    style={"textAlign": "center"},
+                                ),
+                                dcc.Graph(
+                                    id="Predicted price rate of change Data",
+                                    figure={
+                                        "data": [
+                                            go.Scatter(
+                                                x=appleCloseData[2].index,
+                                                y=appleCloseData[3]["Predictions"],
+                                            )
+                                        ],
+                                        "layout": go.Layout(
+                                            title="scatter plot",
+                                            xaxis={"title": "Date"},
+                                            yaxis={"title": "Rate"},
+                                        ),
+                                    },
+                                ),
                             ]
                         )
                     ],
                 ),
-                dcc.Tab(
-                    label="Facebook Stock Data",
-                    children=[
-                        html.Div(
-                            [
-                                html.H1(
-                                    "Stocks High vs Lows", style={"textAlign": "center"}
-                                ),
-                                dcc.Dropdown(
-                                    id="my-dropdown",
-                                    options=[
-                                        {"label": "Tesla", "value": "TSLA"},
-                                        {"label": "Apple", "value": "AAPL"},
-                                        {"label": "Facebook", "value": "FB"},
-                                        {"label": "Microsoft", "value": "MSFT"},
-                                    ],
-                                    multi=True,
-                                    value=["FB"],
-                                    style={
-                                        "display": "block",
-                                        "margin-left": "auto",
-                                        "margin-right": "auto",
-                                        "width": "60%",
-                                    },
-                                ),
-                                dcc.Graph(id="highlow"),
-                                html.H1(
-                                    "Stocks Market Volume",
-                                    style={"textAlign": "center"},
-                                ),
-                                dcc.Dropdown(
-                                    id="my-dropdown2",
-                                    options=[
-                                        {"label": "Tesla", "value": "TSLA"},
-                                        {"label": "Apple", "value": "AAPL"},
-                                        {"label": "Facebook", "value": "FB"},
-                                        {"label": "Microsoft", "value": "MSFT"},
-                                    ],
-                                    multi=True,
-                                    value=["FB"],
-                                    style={
-                                        "display": "block",
-                                        "margin-left": "auto",
-                                        "margin-right": "auto",
-                                        "width": "60%",
-                                    },
-                                ),
-                                dcc.Graph(id="volume"),
-                            ],
-                            className="container",
-                        ),
-                    ],
-                ),
+                # dcc.Tab(
+                #     label="Tesla Stock Data",
+                #     children=[
+                #         html.Div(
+                #             [
+                #                 html.H2(
+                #                     "Actual closing price",
+                #                     style={"textAlign": "center"},
+                #                 ),
+                #                 dcc.Graph(
+                #                     id="Actual Tesla Data",
+                #                     figure={
+                #                         "data": [
+                #                             go.Scatter(
+                #                                 x=teslaCloseData[0].index,
+                #                                 y=teslaCloseData[1]["Close"],
+                #                                 mode="markers",
+                #                             )
+                #                         ],
+                #                         "layout": go.Layout(
+                #                             title="scatter plot",
+                #                             xaxis={"title": "Date"},
+                #                             yaxis={"title": "Closing Rate"},
+                #                         ),
+                #                     },
+                #                 ),
+                #                 html.H2(
+                #                     "LSTM Predicted closing price",
+                #                     style={"textAlign": "center"},
+                #                 ),
+                #                 dcc.Graph(
+                #                     id="Predicted Data",
+                #                     figure={
+                #                         "data": [
+                #                             go.Scatter(
+                #                                 x=teslaCloseData[0].index,
+                #                                 y=teslaCloseData[1]["Predictions"],
+                #                                 mode="markers",
+                #                             )
+                #                         ],
+                #                         "layout": go.Layout(
+                #                             title="scatter plot",
+                #                             xaxis={"title": "Date"},
+                #                             yaxis={"title": "Closing Rate"},
+                #                         ),
+                #                     },
+                #                 ),
+                #             ]
+                #         )
+                #     ],
+                # ),
+                # dcc.Tab(
+                #     label="Facebook Stock Data",
+                #     children=[
+                #         html.Div(
+                #             [
+                #                 html.H1(
+                #                     "Stocks High vs Lows", style={"textAlign": "center"}
+                #                 ),
+                #                 dcc.Dropdown(
+                #                     id="my-dropdown",
+                #                     options=[
+                #                         {"label": "Tesla", "value": "TSLA"},
+                #                         {"label": "Apple", "value": "AAPL"},
+                #                         {"label": "Facebook", "value": "FB"},
+                #                         {"label": "Microsoft", "value": "MSFT"},
+                #                     ],
+                #                     multi=True,
+                #                     value=["FB"],
+                #                     style={
+                #                         "display": "block",
+                #                         "margin-left": "auto",
+                #                         "margin-right": "auto",
+                #                         "width": "60%",
+                #                     },
+                #                 ),
+                #                 dcc.Graph(id="highlow"),
+                #                 html.H1(
+                #                     "Stocks Market Volume",
+                #                     style={"textAlign": "center"},
+                #                 ),
+                #                 dcc.Dropdown(
+                #                     id="my-dropdown2",
+                #                     options=[
+                #                         {"label": "Tesla", "value": "TSLA"},
+                #                         {"label": "Apple", "value": "AAPL"},
+                #                         {"label": "Facebook", "value": "FB"},
+                #                         {"label": "Microsoft", "value": "MSFT"},
+                #                     ],
+                #                     multi=True,
+                #                     value=["FB"],
+                #                     style={
+                #                         "display": "block",
+                #                         "margin-left": "auto",
+                #                         "margin-right": "auto",
+                #                         "width": "60%",
+                #                     },
+                #                 ),
+                #                 dcc.Graph(id="volume"),
+                #             ],
+                #             className="container",
+                #         ),
+                #     ],
+                # ),
             ],
         ),
     ]
---
+++ tesla_close.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from keras.models import Sequential  # Deeplearing API
+from keras.layers import LSTM, Dropout, Dense
+
+def train():
+    scaler=MinMaxScaler(feature_range=(0,1)) #Scale value from 0 -> 1
+
+    df=pd.read_csv("stock_data.csv")
+    df=df.loc[df['Stock'] == 'TSLA']
+    df["Date"]=pd.to_datetime(df.Date,format="%m/%d/%Y")
+    df.index=df['Date'] #Set index dataframe = date
+
+    data=df.sort_index(ascending=True,axis=0) #Sort index (date) ascending
+    new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close']) #Create new dataframe with Date and Close columns
+
+    #Copy from data to new_dataset (Date, Close)
+    for i in range(0,len(data)):
+        new_dataset["Date"][i]=data['Date'][i]
+        new_dataset["Close"][i]=data["Close"][i]
+
+    new_dataset.index=new_dataset.Date #Set index = Date
+    new_dataset.drop("Date",axis=1,inplace=True) #Delete comlumn Date, axis = 1 means column, inplace = true means do operation inplace and return None
+
+    final_dataset=new_dataset.values  #[[0],[1]]
+
+    train_data=final_dataset[0:1485,:]
+    valid_data=final_dataset[1485:,:]
+
+    scaler=MinMaxScaler(feature_range=(0,1))
+    scaled_data=scaler.fit_transform(final_dataset)
+
+    x_train_data,y_train_data=[],[]
+
+    for i in range(60,len(train_data)):
+        x_train_data.append(scaled_data[i-60:i,0])
+        y_train_data.append(scaled_data[i,0])
+        
+    x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
+
+    x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
+
+    lstm_model=Sequential()
+    lstm_model.add(LSTM(units=50,return_sequences=True,input_shape=(x_train_data.shape[1],1)))
+    lstm_model.add(LSTM(units=50))
+    lstm_model.add(Dense(1))
+
+    lstm_model.compile(loss='mean_squared_error',optimizer='adam')
+    lstm_model.fit(x_train_data,y_train_data,epochs=10,batch_size=5,verbose=2)
+
+    inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
+    inputs_data=inputs_data.reshape(-1,1)
+    inputs_data=scaler.transform(inputs_data)
+
+    X_test=[]
+    for i in range(60,inputs_data.shape[0]):
+        X_test.append(inputs_data[i-60:i,0])
+    X_test=np.array(X_test)
+
+    X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+    closing_price=lstm_model.predict(X_test)
+    closing_price=scaler.inverse_transform(closing_price)
+
+    lstm_model.save("tesla_lstm_model.h5")
+
+    train_data=new_dataset[:1485]
+    valid_data=new_dataset[1485:]
+    valid_data['Predictions']=closing_price
+
+    return [train_data, valid_data]
